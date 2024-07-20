c1f375b02c765b48b1507a4f269d0c7e8cfccea8
Dwi Satrio W <31796624+dwisatriow@users.noreply.github.com>
2024-07-21 03:52:20 +0700

    learn(form-auth): auth actions

---
+++ actions/auth.ts
@@ -0,0 +1,28 @@
+'use server'
+import { signup } from '@/utils/authTools'
+import { COOKIE_NAME } from '@/utils/constants'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
+
+const authSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+export const registerUser = async (prevState: any, formData: FormData) => {
+  const data = authSchema.parse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  try {
+    const { token } = await signup(data)
+    cookies().set(COOKIE_NAME, token)
+  } catch (e) {
+    console.error(e)
+    return { message: 'Failed to sign you up' }
+  }
+
+  redirect('/dashboard')
+}
